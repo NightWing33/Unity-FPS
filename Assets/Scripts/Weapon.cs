5e205709a1da09f188d94093b29a1b41240229da
NightWing33 <teodor.ilie2102@stud.acs.upb.ro>
2026-04-29 10:23:24 +0300

    Weapon logic

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using StarterAssets;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] ParticleSystem muzzleFlash;
+
+    StarterAssetsInputs starterAssetsInputs;
+
+    void Awake()
     {
-        
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
     }
 
-    // Update is called once per frame
     void Update()
-{
+    {
+        if (!starterAssetsInputs.shoot) return;
 
-}
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        {
+            Debug.Log(hit.collider.name);
+            starterAssetsInputs.ShootInput(false);
+        }
+    }
 }
